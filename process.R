fe465c508c475ef0f2856685e46a4f740fa13e83
paynesa <paynesa@sas.upenn.edu>
2020-12-11 19:20:33 -0500

    more graphing

--- process.R
+++ process.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+library(ggplot2)
 #Read in the data from the CSV files 
 english <- read.csv("eng_vot_data.csv")
 spanish <- read.csv("spa_vot_data.csv")
@@ -24,13 +24,17 @@ b_spa <- spanish %>% filter(Consonant == 'b')
 p_eng <- english %>% filter(Consonant == 'p')
 p_spa <- spanish %>% filter(Consonant == 'p')
 
-#get the means
-means_english <- c(mean(d_eng$VOT), mean(t_eng$VOT), mean(b_eng$VOT), mean(p_eng$VOT))
-means_spanish <- c(mean(d_spa$VOT), mean(t_spa$VOT), mean(b_spa$VOT), mean(p_spa$VOT))
-table_means <- c(means_english, means_spanish)
+
+specie <- c(rep("d", 2), rep("t", 2), rep("b", 2), rep("p", 2))
+condition <- rep(c("Spanish" , "English") , 4)
+value <- c(mean(d_spa$VOT), mean(d_eng$VOT), mean(t_spa$VOT),mean(t_eng$VOT),  mean(b_spa$VOT),mean(b_eng$VOT),  mean(p_spa$VOT),  mean(p_eng$VOT))
+data <- data.frame(specie,condition,value)
 
 
 
-barplot(table_means, main="Car Distribution by Gears and VS",
-        xlab="Number of Gears", col=c("darkblue","red"),
-        beside=TRUE)
+
+theme_set(theme_bw())
+# Grouped
+ggplot(data, aes(fill=condition, y=value, x=specie)) + 
+  geom_bar(position="dodge", stat="identity") + xlab("") + ylab("VOT (seconds)") + ggtitle("Hello")
+
