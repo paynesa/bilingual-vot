9c47f275b739a4b13fe1dbfd1c866e68f70b5c48
paynesa <paynesa@sas.upenn.edu>
2020-12-11 19:40:54 -0500

    got first plot

--- process.R
+++ process.R
@@ -28,8 +28,9 @@ p_spa <- spanish %>% filter(Consonant == 'p')
 specie <- c(rep("d", 2), rep("t", 2), rep("b", 2), rep("p", 2))
 condition <- rep(c("Spanish" , "English") , 4)
 value <- c(mean(d_spa$VOT), mean(d_eng$VOT), mean(t_spa$VOT),mean(t_eng$VOT),  mean(b_spa$VOT),mean(b_eng$VOT),  mean(p_spa$VOT),  mean(p_eng$VOT))
+sd <- c(sd(d_spa$VOT), sd(d_eng$VOT), sd(t_spa$VOT), sd(t_eng$VOT), sd(b_spa$VOT), sd(b_eng$VOT), sd(p_spa$VOT),  sd(p_eng$VOT))
 data <- data.frame(specie,condition,value)
 theme_set(theme_bw())
 ggplot(data, aes(fill=condition, y=value, x=specie)) + 
-  geom_bar(position="dodge", stat="identity") + xlab("") + scale_fill_brewer(palette="Dark2")+ ylab("VOT (seconds)") + ggtitle("Average VOT across 3 Simultaneous Spanish-English Bilingual 2-year-olds")
+  geom_bar(position="dodge", stat="identity") + xlab("") + scale_fill_brewer(palette="Dark2")+ ylab("VOT (seconds)") +  ggtitle("Average VOT across 3 Simultaneous Spanish-English Bilingual 2-year-olds") +geom_errorbar(aes(ymin=value-sd, ymax=value+sd), size=0.25, width=0.25, position=position_dodge(.9)) 
 
